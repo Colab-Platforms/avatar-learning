2f68d45b8d248a419d465c3c8e6886be9a303751
vinayakpotdar79 <vinayakpotdar7977@gmail.com>
2026-07-27 17:25:12 +0530

    added microsoft clarity to track users

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -49,6 +49,13 @@ export default function RootLayout({
             src="https://www.facebook.com/tr?id=1593452955528348&ev=PageView&noscript=1"
           />
         </noscript>
+        <Script id="ms-clarity" strategy="afterInteractive">{`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "xsy8mmymts");
+        `}</Script>
       </head>
       <body>
         <StoreProvider>
